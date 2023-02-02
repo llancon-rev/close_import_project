f826fbfb774b1e89dddb94ebd02395afce274c64
llancon <rdzalb@gmail.com>
2023-02-02 15:43:53 -0500

    Adds csv module

---
+++ import.py
@@ -0,0 +1,21 @@
+# importing csv module
+import csv
+
+# csv file name
+filename = "aapl.csv"
+
+# initializing the titles and rows list
+fields = []
+rows = []
+
+# reading csv file
+with open(filename, 'r') as csvfile:
+	# creating a csv reader object
+	csvreader = csv.reader(csvfile)
+	
+	# extracting field names through first row
+	fields = next(csvreader)
+
+	# extracting each data row one by one
+	for row in csvreader:
+		rows.append(row)
