e79fcf7aa892d5cce86a3e2798ab28288e77bd6e
llancon <rdzalb@gmail.com>
2023-02-02 16:35:55 -0500

    Adds dotenv

--- import.py
+++ import.py
@@ -1,15 +1,23 @@
 # importing csv module
 import csv
 
-# csv file name
-filename = "aapl.csv"
+# Load environment variables
+from dotenv import load_dotenv
+import os
+load_dotenv('vault.env')
+
+# Set API Key
+api_key = os.getenv("CLOSE_API_KEY")
+
+# Source CSV file name
+source_file = "aapl.csv"
 
 # initializing the titles and rows list
 fields = []
 rows = []
 
 # reading csv file
-with open(filename, 'r') as csvfile:
+with open(source_file, 'r') as csvfile:
 	# creating a csv reader object
 	csvreader = csv.reader(csvfile)
 	
@@ -19,3 +27,6 @@ with open(filename, 'r') as csvfile:
 	# extracting each data row one by one
 	for row in csvreader:
 		rows.append(row)
+
+
+print(f"test: {api_key}!!")
